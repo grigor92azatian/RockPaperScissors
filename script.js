9701607e37fb248acf06c5fd6265820f577c5263
Grigor Azatian <grigorazatian@yahoo.com>
2021-04-05 23:32:48 -0700

    rock, paper, scissor game completed and working

--- script.js
+++ script.js
@@ -11,19 +11,69 @@ function computerPlay(){        //a function that randomly returns either Rock, 
     let randomNum = random123();
         //Translate the random number to its corresponding play and return it
     if(randomNum === rock){
-        return "Rock";
+        return "rock";
     }else if(randomNum === paper){
-        return "Paper";
+        return "paper";
     }else if(randomNum === scissors){
-        return "Scissors";
+        return "scissors";
     }
 }
 
-function rpsRound(playerSelection, computerSelection){
-        //Ask the user to input their selection and save this into a new variable
-    
-        //Ask the computer for its random selection and store it into a new variable
-        //Compare the two and display the results based on the game's rules
+function userPlay(){        //a function that asks the user for their game play selection
+    let userSelection = prompt("Choose your move. Rock? Paper? Scissors?");     //user input
+    userSelection = userSelection.toLowerCase();        //convert user input to all lowercase in order to make comparison easier
+    return userSelection;
 }
 
+function game(numRounds){       //function that loops the single-round as many times as chosen and keeps score and then displays the final score
+    let userScore=0;          //keeps track of your score
+    let computerScore=0;      //keeps track of computer's score
 
+    function singleRoundRPS(playerSelection, computerSelection){
+        //Compare the two and display the results based on the game's rules
+        switch(true){
+            case (playerSelection=="rock"&&computerSelection=="rock"):
+                console.log("It's a draw! Rock vs Rock.");
+                break;
+            case (playerSelection=="rock"&&computerSelection=="paper"):
+                computerScore++;
+                console.log("You lose! Paper beats rock."); 
+                break;
+            case (playerSelection=="rock"&&computerSelection=="scissors"):
+                userScore++;
+                console.log("You win! Rock beats scissors."); 
+                break;
+            case (playerSelection=="paper"&&computerSelection=="rock"):
+                userScore++;
+                console.log("You win! Paper beats rock."); 
+                break;
+            case (playerSelection=="paper"&&computerSelection=="paper"):
+                console.log("It's a draw! Paper vs paper."); 
+                break;
+            case (playerSelection=="paper"&&computerSelection=="scissors"):
+                computerScore++;
+                console.log("You lose! Scissors beats paper."); 
+                break;
+            case (playerSelection=="scissors"&&computerSelection=="rock"):
+                computerScore++;
+                console.log("You lose! Rock beats scissors."); 
+                break;
+            case (playerSelection=="scissors"&&computerSelection=="paper"):
+                userScore++;
+                console.log("You win! Scissors beats paper."); 
+                break;
+            case (playerSelection=="scissors"&&computerSelection=="scissors"):
+                console.log("It's a draw! Scissors vs scissors."); 
+        }
+    }
+
+    for(let i=0;i<numRounds;i++){
+        singleRoundRPS(userPlay(), computerPlay());
+    }
+
+    console.log("Your score: "+userScore);
+    console.log("Computer's score: "+ computerScore);
+
+}
+
+//game(5);
