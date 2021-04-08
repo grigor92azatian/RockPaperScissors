e6d9a8d9a08dc5853da2db83969cd3cc482022ca
Grigor Azatian <grigorazatian@yahoo.com>
2021-04-08 01:54:14 -0700

    Most of logic has been figured out onto console, next step is DOM manipulation

--- simpleUI.js
+++ simpleUI.js
@@ -1,5 +1,5 @@
 
-function computerPlay(){        //a function that randomly returns either Rock, Paper, or Scissors
+function getComputersPlay(){        //a function that randomly returns either Rock, Paper, or Scissors
 //Assign 1,2,3 to Rock,Paper,Scissors
     let rock = 1;
     let paper = 2;
@@ -19,7 +19,51 @@ function computerPlay(){        //a function that randomly returns either Rock, 
     }
 }
 
+function getWinnerOfRound(usersMove,computersMove){ //returns "user", "computer", or "draw"
+    if(usersMove === "rock"){
+        if(computersMove==="rock"){
+            return "draw";
+        }else if(computersMove==="paper"){
+            return "computer";
+        }else if(computersMove==="scissors"){
+            return "user";
+        }
+    }else if(usersMove==="paper"){
+        if(computersMove==="rock"){
+            return "user";
+        }else if(computersMove==="paper"){
+            return "draw";
+        }else if(computersMove==="scissors"){
+            return "computer";
+        }
+    }else if(usersMove==="scissors"){
+        if(computersMove==="rock"){
+            return "computer";
+        }else if(computersMove==="paper"){
+            return "user";
+        }else if(computersMove==="scissors"){
+            return "draw";
+        }
+    }
+}
 
+function incrementScore(winner){
+    if(winner === "user"){
+        usersScore++;
+    }else if(winner==="computer"){
+        computersScore++;
+    }
+}
+
+function getRoundResults(winner){
+    if(winner==="user"){
+        return "Yay! You won this round!";
+    }else if(winner==="computer"){
+        return "Oh no! The computer won this round!"
+    }else if(winner==="draw"){
+        return "It's a draw! Keep going!";
+    }
+}
 /*
 Simple UI:
 1. display rock paper scissor option buttons and scoreboard
@@ -48,24 +92,43 @@ assign that function to the "click" event
 */
 
 
-let usersPlay;
+
+
 
 let rockButton = document.querySelector(".rock");
-
-rockButton.addEventListener("click", function(e){
-    usersPlay = e.target.innerText;
-    usersPlay = usersPlay.toLowerCase();
-    console.log(usersPlay);
-});
-let className = rockButton.getAttribute("class");
-
+rockButton.addEventListener("click", onClickSingleRound);
 let paperButton = document.querySelector(".paper");
-paperButton.addEventListener("click", function(){
-    usersPlay = paperButton.getAttribute("class");
-    console.log(typeof usersPlay);
-});
+paperButton.addEventListener("click", onClickSingleRound);
+let scissorsButton = document.querySelector(".scissors");
+scissorsButton.addEventListener("click", onClickSingleRound);
+let usersPlay;
+let computersPlay;
+let winnerOfRound;
+let usersScore=0;
+let computersScore=0;
+let resultsOfRound;
 
+function onClickSingleRound(){
+//1. assign the button's class name to usersPlay - this will be the user's move for that round
+    usersPlay = this.getAttribute("class");
+    console.log("User chose: "+usersPlay);
+//2. call upon getComputersPlay and assign the return value to computersPlay - this is the computer's move
+    computersPlay = getComputersPlay();
+    console.log("Computer chose: "+computersPlay);
+//3. call upon getWinnerOfRound and assign return to winnerOfRound - winner of round determined
+    winnerOfRound = getWinnerOfRound(usersPlay,computersPlay);
+    console.log("Winner: "+winnerOfRound);
+//4. call upon incrementScore and increase the score of whoever won the round
+    incrementScore(winnerOfRound);
+    console.log("User's score: "+usersScore);
+    console.log("Computer's score: "+computersScore);
+//5. display result of round
+    resultsOfRound=getRoundResults(winnerOfRound);
+    console.log(resultsOfRound);
+    console.log("\n");
+}
 
+//use do...while to run loop until either score reaches 5....as soon as it reaches 5, display winner of game
 
 
 
